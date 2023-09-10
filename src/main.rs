064d138b9a5833da1a0261f5ca1f479ab8b2e3e2
eramoss <edulramos@outlook.com>
2023-09-10 18:11:03 -0300

    create routers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use axum::{routing::get, Router};
+mod person;
+mod routes;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use person::*;
+use routes::*;
+use std::env;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/contagem-pessoas", get(|| amount_of_people()))
+        .route("/pessoas/:id", get(|| search_person_by_id()))
+        .route("/pessoas", post(|person: String| create_person(person)))
+        .route(
+            "/pessoas",
+            get(|query: String| search_people_by_term(query)),
+        );
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    // run it with hyper on localhost:3000s
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
---
+++ src/routes.rs
@@ -0,0 +1,14 @@
+use axum::response::IntoResponse;
+
+pub async fn search_people_by_term(query: String) -> impl IntoResponse {
+    todo!()
+}
+pub async fn search_person_by_id() -> impl IntoResponse {
+    todo!()
+}
+pub async fn create_person(person: String) -> impl IntoResponse {
+    todo!()
+}
+pub async fn amount_of_people() -> impl IntoResponse {
+    todo!()
+}
